0c3e29c1210314fae89043861e697f2ad7b18698
Sean <seantsusmilch@gmail.com>
2022-11-03 14:27:13 -0500

    🌠Initialize

---
+++ src/steppermotors.c
@@ -0,0 +1,49 @@
+// Stepper3.C
+// Position control of a stepper motor
+
+// Global Variables
+const unsigned char MSG1[16] = "REF             ";
+const unsigned char MSG2[16] = "STEP            ";
+
+unsigned char TABLE[4] = {1, 2, 4, 8};
+
+// Subroutine Declarations
+#include <pic18.h>
+#include "LCD_PortD.C"
+        
+
+// main routine
+void main(void) {
+    unsigned int i, REF, STEP, MS;
+
+    TRISA = 0;
+    TRISB = 0xFF;
+    TRISC = 0;
+    ADCON1 = 0x0F;
+
+    STEP = 0;
+    REF   = 0;
+
+    LCD_Init();
+    LCD_Move(0,0);  for (i=0; i<16; i++) LCD_Write(MSG1[i]);
+    LCD_Move(1,0);  for (i=0; i<16; i++) LCD_Write(MSG2[i]);
+    Wait_ms(100);
+
+    MS = 50;
+
+    while(1) {
+        if(RB0) REF = 0;
+        if(RB1) REF = 25;
+        if(RB2) REF = 50;
+        if(RB3) REF = 75;
+        if(RB4) REF = 100;
+
+        if (STEP < REF) STEP = STEP + 1;
+        if (STEP > REF) STEP = STEP - 1;
+
+        PORTC = TABLE[STEP % 4];
+        LCD_Move(0,8);  LCD_Out(REF, 5, 0);
+        LCD_Move(1,8);  LCD_Out(STEP, 5, 0);
+        Wait_ms(30);
+    }
+}
---
+++ tools/build.ps1
@@ -0,0 +1,31 @@
+# Executing: "C:\Program Files (x86)\HI-TECH Software\PICC-18\PRO\9.63\bin\picc18.exe" --pass1 "C:\Users\seant\Nextcloud\Documents\School\2022 Fall\ECE 376 Embedded Systems\MPLABC\1234.c" -q --chip=18F4620 -P --runtime=default,+clear,+init,-keep,-download,+stackwarn,-config,+clib,-plib --opt=default,+asm,-speed,+space,9 --warn=0 -D__DEBUG=1 --mode=pro -g --asmlist "--errformat=Error   [%n] %f; %l.%c %s" "--msgformat=Advisory[%n] %s" "--warnformat=Warning [%n] %f; %l.%c %s" 
+# Executing: "C:\Program Files (x86)\HI-TECH Software\PICC-18\PRO\9.63\bin\picc18.exe" -omplabc.cof -mmplabc.map --summary=default,-psect,-class,+mem,-hex --output=default,-inhx032 --codeoffset=0x800 1234.p1 --chip=18F4620 -P --runtime=default,+clear,+init,-keep,-download,+stackwarn,-config,+clib,-plib --opt=default,+asm,-speed,+space,9 --warn=0 -D__DEBUG=1 --mode=pro -g --asmlist "--errformat=Error   [%n] %f; %l.%c %s" "--msgformat=Advisory[%n] %s" "--warnformat=Warning [%n] %f; %l.%c %s" 
+
+$fullPath = $args[0]
+$fileNameNoDir = $fullPath.Split("\")[-1]
+$fileNameNoExt = $fileNameNoDir.Split(".")[0]
+Write-Output "$fileNameNoDir"
+Write-Output "$fileNameNoExt"
+if($fileNameNoDir -like ""){
+    Write-Output "No file in arguments! Exiting..."
+    exit
+}
+
+Write-Output "Compiling $fileNameNoDir"
+
+$tmp = "./tmp"
+$comp_exe = "C:\Program Files (x86)\HI-TECH Software\PICC-18\PRO\9.63\bin\picc18.exe"
+
+if(Test-Path -Path $tmp){
+    Set-Location $tmp
+}else{
+    Set-Location "../$tmp"
+}
+
+Write-Output "$fullPath"
+& $comp_exe --pass1 "$fullPath" -q --chip=18F4620 -P --runtime=default,+clear,+init,-keep,-download,+stackwarn,-config,+clib,-plib --opt=default,+asm,-speed,+space,9 --warn=0 -D__DEBUG=1 --mode=pro -g --asmlist "--errformat=Error   [%n] %f; %l.%c %s" "--msgformat=Advisory[%n] %s" "--warnformat=Warning [%n] %f; %l.%c %s"
+
+& $comp_exe --summary=default,-psect,-class,+mem,-hex --output=default,-inhx032 --codeoffset=0x800 "$fileNameNoExt.p1" --chip=18F4620 -P --runtime=default,+clear,+init,-keep,-download,+stackwarn,-config,+clib,-plib --opt=default,+asm,-speed,+space,9 --warn=0 -D__DEBUG=1 --mode=pro -g --asmlist "--errformat=Error   [%n] %f; %l.%c %s" "--msgformat=Advisory[%n] %s" "--warnformat=Warning [%n] %f; %l.%c %s"
+# & $comp_exe -omplabc.cof -mmplabc.map --summary=default,-psect,-class,+mem,-hex --output=default,-inhx032 --codeoffset=0x800 1234.p1 --chip=18F4620 -P --runtime=default,+clear,+init,-keep,-download,+stackwarn,-config,+clib,-plib --opt=default,+asm,-speed,+space,9 --warn=0 -D__DEBUG=1 --mode=pro -g --asmlist "--errformat=Error   [%n] %f; %l.%c %s" "--msgformat=Advisory[%n] %s" "--warnformat=Warning [%n] %f; %l.%c %s"
+
+Move-Item "./$fileNameNoExt.hex" ../build -Force
